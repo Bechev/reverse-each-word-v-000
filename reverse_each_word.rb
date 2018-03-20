6e1e42c343e064abd770ed7730aa15279bf39930
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-20 11:09:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ end
 #   string.split.collect {|word| word.reverse}.join (" ")
 # end
 def reverse_each_word(string)
-
+  binding.pry
   reverse_array = string.split(" ")
   reverse_array.collect do |word|
     word.reverse
